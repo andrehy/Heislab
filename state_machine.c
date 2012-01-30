cb3b138144321b56a203a6a05d360ce7e27c4057
thomsten <stenersen.thomas@gmail.com>
2012-01-30 12:49:02 +0100

    Update state_machine.c

--- state_machine.c
+++ state_machine.c
@@ -5,19 +5,33 @@
   This is the state machine for the elevator. All input and orders should be
   taken care of in this function according to the current state of the
   elevator.
+  
+  int order_list[N_FLOORS] = 0;
+  
+  order_list[zhe_floor] == 0 => no order in zhe_floor
+  order_list[zhe_floor] == 1 => external 'up' order in zhe_floor
+  order_list[zhe_floor] == -1 => external 'down' order in zhe_floor
+  order_list[zhe_floor] == 2 => internal order in zhe_floor
+  
 */
 
 
 int state_machine(int state){
   switch (state) {
     case 0: // UP
-      // DO SOMETHING test
+      /*
+        Check floor sensor. If floor is reached, check orderlist. Stop if order_list[zhe_floor] == 1 or 2.
+      */
       break;
     case 1: // DOWN
-      // DO SOMETHING
+      /*
+        Check floor sensor. If floor is reached, check orderlist. Stop if order_list[zhe_floor] == -1 or 2.
+      */
       break;
     case 2: // AT_FLOOR
-      // DO SOMETHING
+      /*
+        Check obstruction() etc. and change state accordingly..
+      */
       break;
     case 3: // EMERGENCY
       // DO SOMETHING
